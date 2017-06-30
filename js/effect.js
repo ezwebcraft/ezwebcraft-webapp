77e0090315eef772d36e0d6c7063f21506440b8f
Elvin Ramirez <real8686@gmail.com>
2017-06-29 21:58:23 -0400

    feat: add badge api

--- js/effect.js
+++ js/effect.js
@@ -66,3 +66,44 @@ function createPie(dataElement, pieElement) {
   }
 }
 createPie(".pieID.legend", ".pieID.pie");
+
+// Treehouse script and plugin for badges
+
+
+// When Document is ready, build treehouse Badge Widget
+$(document).ready(function () {
+
+  // Replace the value for var 'e' with your Treehouse Username
+  var e = "elvinramirez",
+
+  // Treehouse Json
+  t = "https://teamtreehouse.com/" + e + ".json",
+
+  // Badges JQuery Identifier
+  n = $("#badges"),
+
+  // Badges Array
+  r = [],
+
+  // Badges Count
+  i = 0;
+
+  // Json Parse Treehouse User Badges Info
+  $.getJSON(t, function (e) {
+
+    // User Json Parse Select Badges Info
+  	var t = e.badges;
+
+    // Construct Each badge's HTML
+  	$.each(t, function (e, t) {
+  	 r += '<li><a href="' + t.url + '" target="_blank"><img src="' + t.icon_url + '" alt="' + t.name + '" title="' + t.name + '"/></a></li>';
+  		i++
+  	});
+
+    // Append Badge to #badges
+  	n.append(r);
+
+    // Header Badges count generator
+  	$("#treehouse-count").append('Total (' + i + ') badges earned at Treehouse!');
+  });
+});
