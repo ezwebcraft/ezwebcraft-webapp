7df72951ab903f26a36f80946c36a4d3f029e932
Elvin Ramirez <real8686@gmail.com>
2017-07-08 18:34:48 -0400

    feat: added api from codeschool course

--- js/effect.js
+++ js/effect.js
@@ -119,3 +119,44 @@ $(document).ready(function () {
   	$("#treehouse-count").append('Total (' + i + ') badges earned at Treehouse!');
   });
 });
+
+$(function() {
+
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Ramirez1900.json',
+    dataType: 'jsonp',
+    success: function(data) {
+      CodeSchool(data.courses.completed)
+    }
+  });
+
+  function CodeSchool(courses) {
+
+    var $badges = $('#badges2');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
+
+});
