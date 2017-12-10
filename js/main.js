c5387585c6579f50ebd4ed3bdaadd66474730480
Elvin Ramirez <real8686@gmail.com>
2017-12-09 22:59:19 -0500

    fix: clean up code

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 $(function() {
 
-  $.ajax({
-    url: 'https://www.codeschool.com/users/Ramirez1900.json',
-    dataType: 'jsonp',
-    success: function(data) {
-      populateWithCourses(data.courses.completed)
-    }
-  });
-
-  function populateWithCourses(courses) {
-
-    var $badges = $('#badges');
-
-    courses.forEach(function(course) {
-
-      $div = $('<div />', {
-        'class': 'course'
-      }).appendTo($badges);
-
-      $('<h3 />', {
-        text: course.title
-      }).appendTo($div);
-
-      $('<img />', {
-        src: course.badge
-      }).appendTo($div);
-
-      $('<a />', {
-        'class': 'btn btn-primary',
-        target: '_blank',
-        href: course.url,
-        text: 'See Course'
-      }).appendTo($div);
-
+    $.ajax({
+        url: 'https://www.codeschool.com/users/Ramirez1900.json',
+        dataType: 'jsonp',
+        success: function(data) {
+            populateWithCourses(data.courses.completed)
+        }
     });
 
-  }
+    function populateWithCourses(courses) {
+
+        var $badges = $('#badges');
+
+        courses.forEach(function(course) {
+
+            $div = $('<div />', {
+                'class': 'course'
+            }).appendTo($badges);
+
+            $('<h3 />', {
+                text: course.title
+            }).appendTo($div);
+
+            $('<img />', {
+                src: course.badge
+            }).appendTo($div);
+
+            $('<a />', {
+                'class': 'btn btn-primary',
+                target: '_blank',
+                href: course.url,
+                text: 'See Course'
+            }).appendTo($div);
+
+        });
+
+    }
 
 });
