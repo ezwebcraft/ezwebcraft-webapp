d6350f62f586a0e283ab42b115e5e20df5da80fe
Elvin Ramirez <real8686@gmail.com>
2017-12-26 13:31:36 -0500

    add: mCustomScrollbar

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,6 +1,11 @@
- $(document).ready(function() {
-     $('#sidebarCollapse').on('click', function() {
-         $('#sidebar').toggleClass('active');
-         $(this).toggleClass('active');
-     });
- });
+$(document).ready(function () {
+
+// $("#sidebar").mCustomScrollbar({
+//       theme: "minimal"
+//  });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
